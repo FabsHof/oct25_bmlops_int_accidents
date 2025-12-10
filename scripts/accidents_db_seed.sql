83eef6b1e6011ea83e6b4f803950a4b898e1900a
.T. <StormyLab@proton.me>
2025-12-10 09:49:31 +0100

    feat: 5 re usable databases for raw and clean data (#35)
    - added docker-compose and Dockerfiles for PostgreSQL files
    - added seeding file
    ---------
    
    Co-authored-by: fabianhofmann <fabianhofmann42@gmail.com>

---
+++ scripts/accidents_db_seed.sql
@@ -0,0 +1,104 @@
+-- Make sure the database does not already exist, then create it and connect to it
+SELECT 'CREATE DATABASE accidents_db'
+WHERE NOT EXISTS (SELECT FROM pg_database WHERE datname = 'accidents_db')\gexec
+\c accidents_db;
+-- ============================================================
+CREATE TABLE IF NOT EXISTS raw_caracteristics (
+    num_acc INTEGER PRIMARY KEY,
+    an INTEGER,
+    mois INTEGER,
+    jour INTEGER,
+    hrmn INTEGER,
+    lum INTEGER,
+    agg INTEGER,
+    int INTEGER,
+    atm INTEGER,
+    col INTEGER,
+    com INTEGER,
+    adr VARCHAR,
+    gps VARCHAR,
+    lat INTEGER,
+    long INTEGER,
+    dep INTEGER
+);
+CREATE TABLE IF NOT EXISTS raw_holidays (
+    id SERIAL PRIMARY KEY,
+    ds DATE,
+    holiday VARCHAR
+);
+CREATE TABLE IF NOT EXISTS raw_places (
+    num_acc INTEGER PRIMARY KEY,
+    catr INTEGER,
+    voie INTEGER,
+    v1 INTEGER,
+    v2 VARCHAR,
+    circ INTEGER,
+    nbv INTEGER,
+    pr INTEGER,
+    pr1 INTEGER,
+    vosp INTEGER,
+    prof INTEGER,
+    plan INTEGER,
+    lartpc INTEGER,
+    larrout INTEGER,
+    surf INTEGER,
+    infra INTEGER,
+    situ INTEGER,
+    env1 INTEGER,
+    FOREIGN KEY (num_acc) REFERENCES raw_caracteristics(num_acc)
+);
+CREATE TABLE IF NOT EXISTS raw_users (
+    user_id SERIAL PRIMARY KEY,
+    num_acc INTEGER,
+    place INTEGER,
+    catu INTEGER,
+    grav INTEGER,
+    sexe INTEGER,
+    trajet INTEGER,
+    secu INTEGER,
+    locp INTEGER,
+    actp INTEGER,
+    etatp INTEGER,
+    an_nais INTEGER,
+    num_veh VARCHAR,
+    FOREIGN KEY (num_acc) REFERENCES raw_caracteristics(num_acc)
+);
+CREATE TABLE IF NOT EXISTS raw_vehicles (
+    num_acc INTEGER,
+    senc INTEGER,
+    catv INTEGER,
+    occutc INTEGER,
+    obs INTEGER,
+    obsm INTEGER,
+    choc INTEGER,
+    manv INTEGER,
+    num_veh VARCHAR,
+    PRIMARY KEY (num_acc, num_veh),
+    FOREIGN KEY (num_acc) REFERENCES raw_caracteristics(num_acc)
+);
+CREATE TABLE IF NOT EXISTS preprocessed_data (
+    user_id SERIAL PRIMARY KEY,
+    num_acc INTEGER,
+    year_ INTEGER,
+    moy_cos FLOAT,
+    moy_sin FLOAT,
+    dow_cos FLOAT,
+    dow_sin FLOAT,
+    hod_cos FLOAT,
+    hod_sin FLOAT,
+    catu INTEGER,
+    grav INTEGER,
+    sex INTEGER,
+    birthyear INTEGER,
+    purpose INTEGER,
+    securiy INTEGER,
+    luminosity INTEGER,
+    weather INTEGER,
+    road_type INTEGER,
+    road_surface INTEGER,
+    latitude INTEGER,
+    longitude INTEGER,
+    holiday BOOLEAN,
+    FOREIGN KEY (user_id) REFERENCES raw_users(user_id),
+    FOREIGN KEY (num_acc) REFERENCES raw_caracteristics(num_acc)
+);
